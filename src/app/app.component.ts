0663e81d18bbcc2decc4359c4c5f197eea6b186e
Julien Byrnes <93954379+afrotastique@users.noreply.github.com>
2024-04-03 17:47:31 -0400

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SpaceX_Missions';
+  title = '101406358-lab-test2-comp3133';
 }
